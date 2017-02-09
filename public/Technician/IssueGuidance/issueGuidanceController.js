88a6fd2995b07ac2e5433a7ce5ee7ceab5639c26
Yiupang <leopangchan@gmail.com>
2017-02-09 01:51:08 -0800

    handled conflicts

--- public/Technician/IssueGuidance/issueGuidanceController.js
+++ public/Technician/IssueGuidance/issueGuidanceController.js
@@ -1,23 +1,16 @@
 app.controller('issueGuidanceController', ['$scope', '$state','logService', '$http', '$rootScope', 'notifyDlg',
   function(scope, state, logSer, http, rscope, noDlg) {
     scope.user = {};
-<<<<<<< HEAD
-    scope.allTypes = ["Laptop", "Desktop", "Smart Phones", "Software"]
-=======
+
     scope.allTypes = ["Laptop", "Desktop", "Smart Phones", "Software", "Tablet"]
     scope.allManufactueres = ["Apple", "Sony", "Samsung", "Google", "Dell", "ASUS"]
-	
-
->>>>>>> 96773cf49b9418f74d7c75c2bb74dec6648598be
     scope.manufactueres = ["Apple", "Sony"]
     scope.models = ["iPhone 6"]
     scope.issues = ["Cracked Screen", "Broken Keyboard"]
     scope.progressBarDisplay = {'background-color':'blue'}
     scope.progressPercentage = "70%"
     scope.progressBarDisplay = {"width": "70%"};
-<<<<<<< HEAD
-=======
-	
+
 	var decideManu = function (type)
 	{
 		if (type === "Desktop")
@@ -26,5 +19,4 @@ app.controller('issueGuidanceController', ['$scope', '$state','logService', '$ht
 		}
 	}
 
->>>>>>> 96773cf49b9418f74d7c75c2bb74dec6648598be
 }]);
