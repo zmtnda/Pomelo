5807e6f70e6d350af7ecde7f72e8f583c4e6b8dc
Yiupang <leopangchan@gmail.com>
2017-04-01 18:26:31 -0700

    finish adding manu and model tags in the admin page

--- public/Admin/adminController.js
+++ public/Admin/adminController.js
@@ -1,18 +1,23 @@
 app.controller('adminController', ['$scope', '$state', 'users', 'servs', '$http', 'notifyDlg', 'registerPopService', 'addServicePop', 'cates',
   function(scope, state, users, servs, http, noDlg, regPopSer, addSerPop, cates) {
 
+    //Display diplay in thumbnaills' lists
     scope.users = users;
     scope.servs = servs;
     scope.cates = cates;
-    scope.formattedCates = cates.map(function(ea){return ea.category})
-    scope.postingModel = "Enter a new category here"
-    scope.selectedCate = undefined; // for dropdown lists
+    scope.manus = undefined;
+    scope.models = undefined
+
+    //Data display in dropdown lists
+    scope.formattedCates = cates.map(function(ea){return JSON.stringify(ea)})
+    scope.formattedManusInModel = undefined
 
     scope.addUser = function(){
       regPopSer = regPopSer.show(scope, "Add an User")
       .then(function(){state.reload()});
     }
 
+    // addCategory tag
     scope.addCategory = function(newCate){
       http.post("cate/", {"newCategory": newCate})
       .then(function(){
@@ -21,12 +26,60 @@ app.controller('adminController', ['$scope', '$state', 'users', 'servs', '$http'
       .catch(function(err){noDlg.show(scope, err, "Error")});
     }
 
-    scope.addManufacturer = function(){
-      //TODO
+    // addManufacturer tag
+    scope.initManData = function(cateData){
+      var parsedCateDataJSON = JSON.parse(cateData)
+
+      http.get("cate/" + parsedCateDataJSON.id_cat + "/manu")
+      .then(function(res){
+        scope.manus = res.data
+      })
+      .catch(function(err){noDlg.show(scope, err, "Error")});
     }
 
-    scope.addModel = function(){
-      //TODO
+    scope.addManufacturer = function(postingManu, selectedCate){
+      var parsedSelectedCateJSON = JSON.parse(selectedCate)
+
+      http.post("cate/" + parsedSelectedCateJSON.id_cat + "/manu", {"newManufacturer": postingManu})
+      .then(function(res){
+        state.reload()
+      })
+      .catch(function(err){noDlg.show(scope, err, "Error")});
+    }
+
+    // addModel tag
+    // It initialize data in the dropdown list of "Select a manufacturer"
+    scope.initManDataInModelTag = function(selectedCate){
+      var parsedCateDataJSON = JSON.parse(selectedCate)
+      scope.models = []
+      
+      http.get("cate/" + parsedCateDataJSON.id_cat + "/manu")
+      .then(function(res){
+        scope.formattedManusInModel = res.data.map(function(ea){return JSON.stringify(ea)})
+      })
+      .catch(function(err){noDlg.show(scope, err, "Error")});
+    }
+
+    scope.addModel = function(postingModel, selectedCate, selectedManu){
+      var parsedCateDataJSON = JSON.parse(selectedCate)
+      var parsedManuDataJSON = JSON.parse(selectedManu)
+
+      http.post("cate/" + parsedCateDataJSON.id_cat + "/" + parsedManuDataJSON.manId + "/model", {"newModel": postingModel})
+      .then(function(res){
+        state.reload()
+      })
+      .catch(function(err){noDlg.show(scope, err, "Error")});
+    }
+
+    scope.initModelData = function(selectedCate, selectedManu){
+      var parsedCateDataJSON = JSON.parse(selectedCate)
+      var parsedManuDataJSON = JSON.parse(selectedManu)
+      console.log("???" + selectedManu)
+      http.get("cate/" + parsedCateDataJSON.id_cat + "/" + parsedManuDataJSON.manId + "/model")
+      .then(function(res){
+        scope.models = res.data
+      })
+      .catch(function(err){noDlg.show(scope, err, "Error")});
     }
 
     scope.postService = function()
