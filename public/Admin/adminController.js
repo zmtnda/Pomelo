376db2681fefdab92bbb6422e26978bdc2a62814
Yiupang <leopangchan@gmail.com>
2017-04-01 19:36:56 -0700

    missing post issue

--- public/Admin/adminController.js
+++ public/Admin/adminController.js
@@ -5,12 +5,15 @@ app.controller('adminController', ['$scope', '$state', 'users', 'servs', '$http'
     scope.users = users;
     scope.servs = servs;
     scope.cates = cates;
-    scope.manus = undefined;
+    scope.manus = undefined
     scope.models = undefined
+    scope.issues = undefined
 
     //Data display in dropdown lists
     scope.formattedCates = cates.map(function(ea){return JSON.stringify(ea)})
     scope.formattedManusInModel = undefined
+    scope.formattedManusInIssue = undefined
+    scope.formattedModelInIssue = undefined
 
     scope.addUser = function(){
       regPopSer = regPopSer.show(scope, "Add an User")
@@ -52,7 +55,7 @@ app.controller('adminController', ['$scope', '$state', 'users', 'servs', '$http'
     scope.initManDataInModelTag = function(selectedCate){
       var parsedCateDataJSON = JSON.parse(selectedCate)
       scope.models = []
-      
+
       http.get("cate/" + parsedCateDataJSON.id_cat + "/manu")
       .then(function(res){
         scope.formattedManusInModel = res.data.map(function(ea){return JSON.stringify(ea)})
@@ -74,7 +77,7 @@ app.controller('adminController', ['$scope', '$state', 'users', 'servs', '$http'
     scope.initModelData = function(selectedCate, selectedManu){
       var parsedCateDataJSON = JSON.parse(selectedCate)
       var parsedManuDataJSON = JSON.parse(selectedManu)
-      console.log("???" + selectedManu)
+
       http.get("cate/" + parsedCateDataJSON.id_cat + "/" + parsedManuDataJSON.manId + "/model")
       .then(function(res){
         scope.models = res.data
@@ -82,36 +85,46 @@ app.controller('adminController', ['$scope', '$state', 'users', 'servs', '$http'
       .catch(function(err){noDlg.show(scope, err, "Error")});
     }
 
-    scope.postService = function()
-    {
-      //TODO
-      http.post("User/" + 0 + "/Serv", scope.field + "/?admin=true")
-      .then(function(){state.go('admin');})
-      .catch(function(err){noDlg.show(scope, err, "Error")});
-    }
+    // addIssue tag
+    scope.initManDataInIssueTag = function(selectedCate){
+      var parsedCateDataJSON = JSON.parse(selectedCate)
+      scope.models = []
 
-    scope.deleteUser = function(id){
-      //TODO
-      http.delete("User/" + id)
-      .then(function(){
-          state.reload();
-      })
-      .catch(function(err){noDlg.show(scope, err, "Error")});
-     }
-
-    scope.modifyUser = function(){
-      //TODO
-    }
-
-    scope.deleteService = function(id){
-      http.delete("Serv/" + id)
-      .then(function(){
-          state.reload();
+      http.get("cate/" + parsedCateDataJSON.id_cat + "/manu")
+      .then(function(res){
+        scope.formattedManusInIssue = res.data.map(function(ea){return JSON.stringify(ea)})
       })
       .catch(function(err){noDlg.show(scope, err, "Error")});
     }
 
-    scope.modifyService = function(id){
-        //TODO
+    scope.initModelDataInIssueTag = function(selectedCate, selectedManu){
+      var parsedCateDataJSON = JSON.parse(selectedCate)
+      var parsedManuDataJSON = JSON.parse(selectedManu)
+
+      http.get("cate/" + parsedCateDataJSON.id_cat + "/" + parsedManuDataJSON.manId + "/model")
+      .then(function(res){
+        scope.formattedModelInIssue = res.data.map(function(ea){return JSON.stringify(ea)})
+      })
+      .catch(function(err){noDlg.show(scope, err, "Error")});
+    }
+
+    scope.initIssueData = function(selectedModel){
+      var parsedModelDataJSON = JSON.parse(selectedModel)
+
+      http.get("cate/" + parsedModelDataJSON.modelId + "/issues")
+      .then(function(res){
+        scope.issues = res.data
+      })
+      .catch(function(err){noDlg.show(scope, err, "Error")});
+    }
+
+    scope.addIssue = function(postingIssue, selectedModel){
+      var parsedModelDataJSON = JSON.parse(selectedModel)
+
+      http.post("cate/" + parsedModelDataJSON.modelId + "/issues", {"newIssue": postingIssue})
+      .then(function(res){
+        state.reload()
+      })
+      .catch(function(err){noDlg.show(scope, err, "Error")});
     }
 }])
