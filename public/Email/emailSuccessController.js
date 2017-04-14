b50db5658a2ac585df22a9f0e8cb3b4901874cc3
Yiupang <leopangchan@gmail.com>
2017-04-14 10:48:40 -0700

    finish redirection

--- public/Email/emailSuccessController.js
+++ public/Email/emailSuccessController.js
@@ -1,12 +1,12 @@
-app.controller('emailSuccessController', ['$rootScope','$scope', '$timeout',
+app.controller('emailSuccessController', ['$rootScope','$scope', '$state','$timeout',
  function(rscope, scope, state, timeout) {
 
-   scope.redirect = function(){
-     timeout(function() {
-       state.go('home');
-     }, 3000)
-   }
-   }
-
-   scope.redirect();
+     scope.redirect = function()
+     {
+         timeout(function() {
+           state.go('home');
+         }, 3000)
+         console.log("redirect")
+      }
+      scope.redirect();
 }])
