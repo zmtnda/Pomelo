da5f2752b5a3b88ffea8d63bbeb4e23448997b90
NghiaNguyen101 <nnguy101@gmail.com>
2017-04-21 17:05:00 -0700

    add upload photo api

--- Routes/Upload/Upload.js
+++ Routes/Upload/Upload.js
@@ -12,6 +12,7 @@ var fs = require("fs");
 router.baseURL = '/Upload';
 
 const S3_BUCKET = 'pomelotech';
+const THUMB_SIZE = 150;
 
 aws.config.update({
   accessKeyId: "AKIAJ4PWCJV5YRUG3R7Q",
@@ -22,75 +23,183 @@ var s3 = bluebird.promisifyAll(new aws.S3());
 
 // format for upload photo
 // https://${S3_BUCKET}.s3.amazonaws.com/${userEmail}/${album}/${date}-${hash}-${filename}
-// var date = new Date().toISOString();
-// date.substr(0, 4) + date.substr(5, 2) + date.substr(8, 2);
 
-var resizeImg = bluebird.promisify(function(input, size, cb) {
-  gm(input).resize(size).toBuffer(function(err, buffer) {
-    if (err) cb(err); else cb(null, buffer);
+
+router.post('/uploadProfile', (req, res) => {
+  var vld = req.validator;
+  var body = {};
+  var logId = req.session.id;
+  var tecId = req.session.tec_id;
+  var username = req.session.email;
+  var changeAvatarQuery = 'UPDATE Portfolio SET avatar=? WHERE tec_id = ?';
+
+  if (!vld.checkPrsOK(logId))
+    return res.status(400).json({response: 'Permission Denied'});
+
+  new multiparty.Form().parse(req, (err, fields, files) => {
+    if (!files.images)
+      return res.status(400).json({success: 0, response: 'No files found'});
+
+    // var username = 'nnguy101@gmail.com';
+    var albumName = 'profile';
+
+    connections.getConnection(res, cnn => {
+      async.waterfall([
+        function (callback) {
+          uploadImages(username, files, albumName, callback);
+        },
+        function (images, callback) {
+          body.profileUrl = images[0].url;
+          cnn.query(changeAvatarQuery, [images[0].url, tecId], callback);
+        }
+      ], function (err, result) {
+        if (err) res.status(400).json(err);
+        else {
+          res.status(200).json({
+            url: body.profileUrl,
+            success: 1
+          });
+        }
+      });
+    });
   });
 });
 
-router.post('/uploadPic', (req, res) => {
+/*
+ * Require 2 fields, medaData and images
+ * Metadata: album, albumId if any, description
+ */
+router.post('/uploadAlbum', (req, res) => {
   var vld = req.validator;
+  var body = {};
   var logId = req.session.id;
+  var username = req.session.email;
 
   if (!vld.checkPrsOK(logId))
     return;
 
   new multiparty.Form().parse(req, (err, fields, files) => {
-    console.log("fields");
-    console.log(fields);
-
     if (!files.images)
       return res.status(400).json({success: 0, response: 'No files found'});
 
-    files.images.forEach(file => {
-      console.log(file.path, file.originalFilename, file.headers['content-type']);
-    });
+    var metaData = JSON.parse(fields.metaData[0]);
 
-    var username = 'nnguy101@gmail.com';
-    var album = 'pomeloTest'; // get from fields/metadata
-    var operations = files.images.map(file => {
-      var filename = generateFileName(username, album, file.originalFilename);
-      return bluebird.join(file, // need to add thumbs later on
-        s3.uploadAsync({
-          Key: filename,
-          Bucket: S3_BUCKET,
-          ACL:"public-read",
-          ContentType: file.headers['content-type'],
-          Body: fs.createReadStream(file.path)
-        }));
-    });
-    bluebird.join(bluebird.all(operations), images => {
-      console.log("inside bluebird image");
-      // console.log(images);
-
-      var items = images.map(image => {
-        fs.unlink(image[0].path); // delete file on local
-        return {
-          url: image[1].Location,
-          thumb: 'TODO',
-          originalFilename: image[0].originalFilename,
-          position: 'TODO'
-        };
+    connections.getConnection(res, cnn => {
+      async.waterfall([
+        function (callback) {
+          createThumbsBuffer(files, callback);
+        },
+        function (thumbBuffers, callback) {
+          uploadThumbBuffer(username, thumbBuffers, metaData.albumName, callback);
+        },
+        function(thumbUrls, callback) {
+          body.thumbUrls = thumbUrls;
+          console.log("come on");
+          console.log(thumbUrls);
+          uploadImages(username, files, metaData.albumName, callback);
+        },
+        function (images, callback) {
+          var i;
+          for (i=0; i<images.length; i++) {
+            images[i].thumb = body.thumbUrls[i].url;
+          }
+          callback(null, images);
+        }
+      ], function (err, result) {
+        if (err) {
+          console.log(err);
+          res.status(400).json(err);
+        } else {
+          res.status(200).json({
+            albumName: metaData.albumName,
+            description: metaData.description,
+            albumId: 'TODO',
+            images: result
+          });
+        }
       });
-
-      console.log("items");
-      console.log(items);
-      return JSON.stringify(items);
-    })
-    .then(output => {
-      console.log("good");
-      res.json({albumName: album, albumId: 'TODO', images: output});
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    })
+      cnn.release();
+    });
   });
 });
 
+function uploadThumbBuffer(username, thumbs, albumName, cb) {
+  var operations = thumbs.map( thumb => {
+    console.log("Thumb")
+    console.log(thumb);
+    var filename = generateFileName(username, albumName, "thumb_"+ thumb[0].originalFilename);
+    return bluebird.join(thumb,
+      s3.uploadAsync({
+        Key: filename,
+        Bucket: S3_BUCKET,
+        ACL: "public-read",
+        ContentType: "image/png",
+        Body: thumb[1]
+      }));
+  });
+
+  bluebird.join(bluebird.all(operations), images => {
+    return images.map(image => {
+      return {
+        url: image[1].Location
+      }
+    });
+  })
+    .then(result => cb(null, result))
+    .catch(err => cb(err));
+}
+
+function createThumbsBuffer(files, cb) {
+
+  var operations = files.images.map( file => {
+    return bluebird.join(file, resizeImg(file.path, THUMB_SIZE));
+  });
+
+  bluebird.join(bluebird.all(operations), thumbBuffers => {
+    console.log(thumbBuffers);
+    return thumbBuffers;
+  })
+    .then(result => cb(null, result))
+    .catch(err => cb(err));
+}
+
+function uploadImages(username, files, albumName, cb) {
+  var operations = files.images.map(file => {
+    var filename = generateFileName(username, albumName, file.originalFilename);
+    return bluebird.join(file,
+      s3.uploadAsync({
+        Key: filename,
+        Bucket: S3_BUCKET,
+        ACL:"public-read",
+        ContentType: file.headers['content-type'],
+        Body: fs.createReadStream(file.path)
+      }));
+  });
+  bluebird.join(bluebird.all(operations), images => {
+
+    var items = images.map(image => {
+      fs.unlink(image[0].path); // delete file on local
+      return {
+        url: image[1].Location,
+        originalFilename: image[0].originalFilename,
+        position: 'TODO',
+        description: 'TODO'
+      };
+    });
+
+    return items;
+  })
+    .then(result => {
+      cb(null, result);
+    })
+    .catch(err => {
+      console.log("errrr??");
+      console.log(err);
+      cb(err);
+    });
+}
+
+// Generate file name with username and album
 function generateFileName(username, album, origFileName) {
   var date = new Date().toISOString();
   var dateString = date.substr(0, 4) + date.substr(5, 2) + date.substr(8, 2);
@@ -98,7 +207,12 @@ function generateFileName(username, album, origFileName) {
 
   return username + '/' + album + '/' + dateString + '_' + id + '_' + origFileName;
 }
-    
 
+// Resize image for thumb
+var resizeImg = bluebird.promisify(function(input, size, cb) {
+  gm(input).resize(size).setFormat('jpeg').toBuffer(function(err, buffer) {
+    if (err) cb(err); else cb(null, buffer);
+  });
+});
 
 module.exports = router;
--- public/TestUpload/uploadController.js
+++ public/TestUpload/uploadController.js
@@ -1,5 +1,8 @@
 app.controller('uploadController', ['$scope', '$location', '$state', '$http', '$rootScope', '$timeout', 'notifyDlg',
   function (scope, location, state, http, rootScope, timeout, notifyDlg) {
+    scope.images = [{url: 'https://s3-us-west-2.amazonaws.com/pomelotech/nnguy101%40gmail.com/pomeloTest/20170421_f12af0f0d91ed7da9569_safe.png'},
+      {url: 'https://s3-us-west-2.amazonaws.com/pomelotech/nnguy101%40gmail.com/pomeloTest/20170420_12620a88d124642f3894_safe.png'}];
+
     scope.uploadedFile = function (element) {
       alert("yo")
       scope.$apply(function (scope) {
@@ -15,18 +18,19 @@ app.controller('uploadController', ['$scope', '$location', '$state', '$http', '$
       });
 
       var metaData = {
-        description: 'whatever',
-        albumName: 'pomeloTech',
+        description: scope.description,
+        albumName: scope.albumName,
         albumId: -1 // -1 if new, > 0 if exists
       };
       fd.append("metaData", JSON.stringify(metaData));
 
-      http.post('/Upload/uploadPic', fd, {
+      http.post('/Upload/uploadAlbum', fd, {
         transformRequest: angular.identity,
         headers: { 'Content-Type': undefined }
       })
       .success(function(res) {
         console.log(res);
+        scope.images = res.images;
       })
       .error(function(err) {
         console.log(err)
